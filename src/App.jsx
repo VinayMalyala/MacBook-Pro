322c773aae3cf7c9269d17a09c0e690e6af156af
Vinay Malyala <srivinay1432@gmail.com>
2024-12-02 12:47:29 +0530

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,15 @@ import MacContainer from './MacContainer';
 
 const App = () => {
   return (
-    <div className='w-full h-screen'>
-      <div className="absolute flex flex-col items-center text-white top-[100px] left-1/2 -translate-x-1/2 font-signika">
+    <div className='w-full h-screen font-signika'>
+
+      <div className="navbar absolute top-0 left-1/2 -translate-x-1/2">
+          {["iPhone", "iPad", "iPods","Air M1", "Air M2"].map((e) => (
+            <a href="" className='text-white'></a>
+          ))}
+      </div>
+
+      <div className="absolute flex flex-col items-center text-white top-[100px] left-1/2 -translate-x-1/2">
           <h3 className='text-7xl tracking-tighter font-[700] masked mb-3'>macbook pro.</h3>
           <h5>Oh so pro!</h5>
 
