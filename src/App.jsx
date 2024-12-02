6b8c9d5d8f65aae3d8421bded4964aa3d8a07e44
Vinay Malyala <srivinay1432@gmail.com>
2024-12-02 12:15:10 +0530

    working on animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Canvas } from '@react-three/fiber'
 import React from 'react'
+import "./style.css";
+import { Environment, OrbitControls, ScrollControls } from '@react-three/drei';
+import MacContainer from './MacContainer';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Canvas camera={{fov: 12, position: [0, -10, 220]}}>
+      {/* <OrbitControls /> */}
+      <Environment files={["https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/2k/studio_small_09_2k.hdr",]} />
+      
+      <ScrollControls pages={2}>
+          <MacContainer />
+      </ScrollControls>
+    </Canvas>
   )
 }
 
